d97f5c86f1b8396921625461811b46c3c4fb625b
Anshaneja <anshaneja05@gmail.com>
2021-10-21 13:52:28 +0530

    Done

--- index.js
+++ index.js
@@ -1,36 +1,66 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const fs = require('fs');
+const express = require('express');
+const { next } = require('cheerio/lib/api/traversing');
 
-let userData = {
-    citations : { 
-        all : 243,
-        since2016 : 31
-    },
-    hindex : { 
-        all : 243,
-        since2016 : 31
-    },
-    i10index : { 
-        all : 243,
-        since2016 : 31
-    },
+const OurApp = express();
 
-};
-let userinfo = [ [1,2],[1,2],[1,2]];
-
-axios.get('https://scholar.google.com/citations?user=pLs3_AMAAAAJ&hl=en')
-    .then((response) => {
+const getData = async function(url){
+    // tableinfo = [ [citations_all , citations_since 2016] , [hindex_all, hindex_since2016 ] , [i10index_all , i10index_since2016] ]
+    let tableinfo = [ [1,2],[1,2],[1,2]];
+    // graphinfo = [ [ year , no_of_citations ] , ... ]
+    let graphinfo = [];
+    try{
+        // gets the html data from the url
+        const response = await axios.get(url);
         const $ = cheerio.load(response.data);
         const name = $("#gsc_prf_in").text();
+
         $("#gsc_rsb_st tbody").children().each((index, elem) => {
             $(elem).children('.gsc_rsb_std').each((j, elem2)=> {
-                userinfo[index][j] = parseInt($(elem2).text());
+                tableinfo[index][j] = parseInt($(elem2).text());
+                
             })
         });
-        console.log(userinfo);
-    })
 
-    .catch((error) => {
+        $(".gsc_md_hist_b .gsc_g_t").each( (index, elem) => {
+            graphinfo.push([$(elem).text()])
+        })
+
+        $(".gsc_g_al").each((index,elem) => {
+            graphinfo[index].push($(elem).text())
+        })
+
+        console.log(graphinfo);
+        console.log(tableinfo);
+        return ({
+            name : name,
+            tableData : tableinfo,
+            graphData : graphinfo
+        })
+    }
+    catch (error){
         console.log(error);
-    });
+        next(error);
+    }
+        
+}
+
+// function start(url) {
+//     return getData(url);
+//   }
+
+OurApp.get('/user/:id', async(req,res) => {
+    const url = 'https://scholar.google.com/citations?user=' + req.params.id + '&hl=en';
+    const data = await getData(url);
+    console.log('express code continues');
+    console.log(data);
+    return res.json(data);
+});
+
+OurApp.get('/', (req,res) => {
+    return res.json({ message: "Server is working!!!!!!" });
+});
+
+OurApp.listen(4000, () => console.log("Server is running"));
+
