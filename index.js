956e4c3e20f02942e4f9ebe297a23e8d5c9243db
Anshaneja <anshaneja05@gmail.com>
2021-10-21 13:55:41 +0530

    comments added

--- index.js
+++ index.js
@@ -5,17 +5,26 @@ const { next } = require('cheerio/lib/api/traversing');
 
 const OurApp = express();
 
+// function which gets the data and returns object of data
 const getData = async function(url){
+    
     // tableinfo = [ [citations_all , citations_since 2016] , [hindex_all, hindex_since2016 ] , [i10index_all , i10index_since2016] ]
     let tableinfo = [ [1,2],[1,2],[1,2]];
+
     // graphinfo = [ [ year , no_of_citations ] , ... ]
     let graphinfo = [];
+
     try{
         // gets the html data from the url
         const response = await axios.get(url);
+
+        // loads the html data into a cheerio function
         const $ = cheerio.load(response.data);
+
+        // uses cheerio function to select name from html
         const name = $("#gsc_prf_in").text();
 
+        // Selects and Traverses the table data and saves data into an array 'table info'
         $("#gsc_rsb_st tbody").children().each((index, elem) => {
             $(elem).children('.gsc_rsb_std').each((j, elem2)=> {
                 tableinfo[index][j] = parseInt($(elem2).text());
@@ -23,22 +32,26 @@ const getData = async function(url){
             })
         });
 
+        // Traverses graph data 'year' and add it to the array 'graphinfo
         $(".gsc_md_hist_b .gsc_g_t").each( (index, elem) => {
             graphinfo.push([$(elem).text()])
         })
-
+        // Traverses no. of citations in that particular year and saves it to array
         $(".gsc_g_al").each((index,elem) => {
             graphinfo[index].push($(elem).text())
         })
 
         console.log(graphinfo);
         console.log(tableinfo);
+
+        // returns data object
         return ({
             name : name,
             tableData : tableinfo,
             graphData : graphinfo
         })
     }
+    // catches and logs error if any
     catch (error){
         console.log(error);
         next(error);
@@ -46,10 +59,13 @@ const getData = async function(url){
         
 }
 
-// function start(url) {
-//     return getData(url);
-//   }
 
+// Route    - /user/:id
+// Des      - To get a user data
+// Access   - Public
+// Method   - GET
+// Params   - id
+// Body     - none
 OurApp.get('/user/:id', async(req,res) => {
     const url = 'https://scholar.google.com/citations?user=' + req.params.id + '&hl=en';
     const data = await getData(url);
